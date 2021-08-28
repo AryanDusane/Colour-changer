9a8da7aea115013fce7184e97621895c49ed0854
AryanDusane <89576911+AryanDusane@users.noreply.github.com>
2021-08-28 14:07:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+
+function setup() {
+  createCanvas(1400,700);
+}
+
+function draw() 
+{
+  background(30);
+if(keyIsDown(RIGHT_ARROW))
+  background("red");
+if(keyIsDown(LEFT_ARROW))
+background("green");
+if(keyIsDown(DOWN_ARROW))
+background("yellow")
+if(keyIsDown(UP_ARROW))
+background("purple")
+}
+
+
+
+
+
